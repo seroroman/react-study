6e22af234839473f22def66c7beb05f655ba3aa2
seroroman <47804854+seroroman@users.noreply.github.com>
2021-11-02 16:19:12 +0900

    state2

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountButton = () => {
     setNum(num + 1);
   };
 
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountButton}>カウントアップ！</button>
+      <button onClick={onClickSwitchShowFlag}>ON/OFF</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(*^^*)</p>}
     </>
   );
 };
