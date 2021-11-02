94d80e0f3fcd5c1547ab18ae4c8247e8c6d60bd1
seroroman <47804854+seroroman@users.noreply.github.com>
2021-11-02 16:06:15 +0900

    props 分割代入

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,6 @@ const App = () => {
   const onClickButton = () => {
     alert("");
   };
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
-  const contentLedyStyle = {
-    color: "pink",
-    fontSize: "18px"
-  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
